60f6bf2e8848d0c5fa5d655aa5f1f442b1ba01db
Henrique-DeSousa <henriquevsousa@hotmail.com>
2021-12-09 00:52:35 +0000

    Created calculator

---
+++ Calculator.py
@@ -0,0 +1,51 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def addition(x, y):
+    return x+y
+
+
+def subtraction(x, y):
+    return x-y
+
+
+def multiplication(x, y):
+    return x*y
+
+
+def division(x, y):
+    return x/y
+
+
+print("Select operation.")
+print("1.Addition")
+print("2.Subtraction")
+print("3.Multiplication")
+print("4.Division")
+
+while True:
+    choice = input("Enter choice(1/2/3/4): ")
+
+    if choice in ('1' '2' '3' '4'):
+        num1 = float(input("First number: "))
+        num2 = float(input("Second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", addition(num1, num2))
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtraction(num1, num2))
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiplication(num1, num2))
+        elif choice == '4':
+            print(num1, "/", num2, "=", division(num1, num2))
+
+        next_calculation = input("Let's go again? (y/n): ")
+        if next_calculation == "no":
+            break
+        elif next_calculation == "n":
+            break
+
+    else:
+        print("Invalid input")
